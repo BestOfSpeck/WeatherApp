ecc1a59947fd4ccd3b8b2a582b9a722282a607e6
Tom <tom.kuestermann76@gmail.com>
2024-01-19 11:48:06 +0100

    add jsdoc

--- storage.js
+++ storage.js
@@ -1,16 +1,20 @@
 const ApiKey = "2ad4eb2c564b452c8dc93631241801";
 const ApiUrl = "https://api.weatherapi.com/v1/current.json";
 
+/**
+ *  Fetches data from the API
+ * @param {location} location location from input value to fetch data from
+ */
 async function fetchData(location) {
-    const url = `${ApiUrl}?key=${ApiKey}&q=${location}`; // Add the "q" parameter with your desired location
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            // Process the data here
-            console.log(data);
-        })
-        .catch((error) => {
-            // Handle any errors here
-            console.error(error);
-        });
+  const url = `${ApiUrl}?key=${ApiKey}&q=${location}`; // Add the "q" parameter with your desired location
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // Process the data here
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle any errors here
+      console.error(error);
+    });
 }
